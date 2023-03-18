86b89318206bc8c8f60c08b5e303b27295835ecb
webmaster6969 <web.master69@yandex.ru>
2023-03-18 13:05:20 +0300

    https://www.youtube.com/watch?v=YnUfan5OHfo&list=PLhBIgBzRsqo-8HgqUHSb8HE8y403C0H3z&index=4&ab_channel=cibergod.ru 03) Создаем код генерации игрового поля Unity3d

--- Assets/Scenes/Scrips/Chanks.cs
+++ Assets/Scenes/Scrips/Chanks.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Chanks : MonoBehaviour
 {
+    public Sprite[] imgs;
+    public int index = 0;
+
+    public void ChangeImgs()
+    {
+        if(imgs.Length > index)
+        {
+            GetComponent<SpriteRenderer>().sprite = imgs[index];
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ChangeImgs();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ChangeImgs();
     }
 }
--- Assets/Scenes/Scrips/GamePole.cs
+++ Assets/Scenes/Scrips/GamePole.cs
@@ -4,10 +4,62 @@ using UnityEngine;
 
 public class GamePole : MonoBehaviour
 {
+
+    public GameObject eLiters, eNums, ePole;
+
+    GameObject[] Liters;
+    GameObject[] Nums;
+    GameObject[,] Pole;
+
+    int lengPole = 10;
+
+    void CreatePole()
+    {
+        Vector3 StartPoze = transform.position;
+
+        float XX = StartPoze.x + 1;
+        float YY = StartPoze.y - 1;
+
+        Liters = new GameObject[lengPole];
+        Nums = new GameObject[lengPole];
+
+        Pole = new GameObject[10, 10];
+
+        for (int Nadpis = 0; Nadpis < lengPole; Nadpis++)
+        {
+            Liters[Nadpis] = Instantiate(eLiters);
+            Liters[Nadpis].transform.position = new Vector3(XX, StartPoze.y, StartPoze.z);
+            Liters[Nadpis].GetComponent<Chanks>().index = Nadpis;
+            XX++;
+
+            Nums[Nadpis] = Instantiate(eNums);
+            Nums[Nadpis].transform.position = new Vector3(StartPoze.x, YY, StartPoze.z);
+            Nums[Nadpis].GetComponent<Chanks>().index = Nadpis;
+            YY--;
+        }
+
+
+        XX = StartPoze.x + 1;
+        YY = StartPoze.y - 1;
+
+        for (int Y = 0; Y < lengPole; Y++)
+        {
+            for (int X = 0; X < lengPole; X++)
+            {
+                Pole[X, Y] = Instantiate(ePole);
+                Pole[X, Y].GetComponent<Chanks>().index = 0;
+                Pole[X, Y].transform.position = new Vector3(XX, YY, StartPoze.z);
+                XX++;
+            }
+            XX = StartPoze.x + 1;
+            YY--;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreatePole();
     }
 
     // Update is called once per frame
